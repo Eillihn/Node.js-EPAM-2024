8278d5b95d4f334b376d50783d8d1a3bf032da6c
EPAM\Hanna_Svirskaya <Hanna_Svirskaya@epam.com>
2024-03-06 21:35:11 +0200

    Task 2. Standard Library src/task2/activityMonitor.ts: - shows the most CPU-intensive process that's running on the system; - program uses system shell command output to retrieve process name, CPU, and memory usage details; - refresh rate is ten times per second; - the program uses only the standard library; - each update will NOT start from the new line. It is always displayed only in one row; - once per minute program appends the output to the log file src/task2/activityMonitor.log in the format <unixtime> : <process info>. If the file doesn't exist - the program creates it. - program supports Linux, macOS, and Windows operating systems.

--- src/index.js
+++
@@ -1,3 +0,0 @@
-const getRandomNumber = () => Math.max(Math.floor(Math.random() * 1000), 1);
-
-export default getRandomNumber;
---
+++ src/task1/index.ts
@@ -0,0 +1,2 @@
+const getRandomNumber = ():number => Math.max(Math.floor(Math.random() * 1000), 1);
+export default getRandomNumber;
